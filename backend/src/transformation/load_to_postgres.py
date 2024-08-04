0b4553cfa5c5b8da38f221a9f707c78eb51c2485
MatheusDelduque <matheusdelduque1@gmail.com>
2024-08-03 23:13:22 -0300

    refactor: Split function into two separate functions in different files

---
+++ backend/src/transformation/load_to_postgres.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+# Load environment variables
+load_dotenv()
+
+def load_to_postgres(json_file):
+    
+    # Read the JSON file into a pandas DataFrame
+    df = pd.read_json(json_file, orient='records', lines=True)
+
+    # Define the database connection parameters
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    db_host = os.getenv('DB_HOST')
+    db_port = os.getenv('DB_PORT')
+    db_name = os.getenv('DB_NAME')
+    table_name = os.getenv('TABLE_NAME')
+
+    # Define the connection string
+    connection_string = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+    # Create the database engine
+    engine = create_engine(connection_string)
+
+    # Save the DataFrame to the database
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+    print(f"Data inserted into the {table_name} table of the {db_name} database.")
+
+if __name__ == "__main__":
+    load_to_postgres('../../data/transformed_data.json')
---
+++ backend/src/transformation/transform_json.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+from datetime import datetime
+
+def transform_json(input_file, output_file):
+
+    # Read the JSONL file into a pandas DataFrame
+    df = pd.read_json(input_file, lines=True)
+
+    # Add a new column with the source URL
+    df["_source"] = "https://lista.mercadolivre.com.br/tenis-corrida-masculino"
+
+    # Add a new column with the collection date
+    df["_collection_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Clean up the data
+    df["brand"] = df["brand"].fillna("")
+    df["reviews_rating_number"] = df["reviews_rating_number"].fillna("0").astype(float)
+    df["old_price"] = df["old_price_reais"].astype(float) + df["old_price_cents"].astype(float).fillna(0) / 100
+    df["new_price"] = df["new_price_reais"].astype(float) + df["new_price_cents"].astype(float).fillna(0) / 100
+    df['reviews_amount'] = df['reviews_amount'].str.replace('(', '').str.replace(')', '')
+    df['reviews_amount'] = df['reviews_amount'].fillna('0').astype(int)
+
+    # Drop unnecessary columns
+    df = df.drop(columns=['old_price_reais', 'old_price_cents', 'new_price_reais', 'new_price_cents'])
+
+    # Save the transformed data to a new JSONL file
+    df.to_json(output_file, orient='records', lines=True, force_ascii=False)
+    print(f"Transformed data saved to {output_file}")
+
+if __name__ == "__main__":
+    transform_json("../../data/data.jsonl", '../../data/transformed_data.json')
