f66dcabb2fd8cf2b6770c52976a5754def1020b0
MatheusDelduque <matheusdelduque1@gmail.com>
2024-08-07 00:03:18 -0300

    feat (ui): Add graphical components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,25 @@
-import { Box, Heading } from "@chakra-ui/react";
-import { TopBrandsChart } from "./components/TopBrandsChart";
+"use client";
+
+import { Box, Flex, Grid, Heading } from "@chakra-ui/react";
+import { BrandsChart } from "./components/BrandsChart";
 import { AppProvider } from "./components/AppProvider";
+import { fetchBrandCount, IBrandCount } from "./apis/fetchBrandCount";
+import { fetchAveragePriceOfEachBrand, IAveragePriceOfEachBrand } from "./apis/fetchAveragePriceOfEachBrand";
+import { fetchBrandAverageRatingNumberReviews, IBrandAverageRatingNumberReviews } from "./apis/fetchBrandAverageRatingNumberReviews";
 
 export default function Home() {
   return (
     <AppProvider>
-      <Box p={4} color={"white"} bgGradient="linear(to-b, black, gray.700)" h={"100vh"} boxShadow={"lg"}>
-        <Heading size="2xl" p={5} display={"flex"} justifyContent={"center"}>
+      <Flex p={4} color={"white"} bgGradient="linear(to-b, black, gray.700)" minH={"100vh"} flexDirection="column">
+        <Heading size="2xl" mt={14} textAlign="center">
           Research about sporty shoes - Mercado Livre
         </Heading>
-        <TopBrandsChart />
-      </Box>
+        <Grid p={5} templateColumns="repeat(3, 1fr)" gap={6} flexGrow={1}>
+          <BrandsChart<IBrandCount> queryKey="brandCount" queryFn={fetchBrandCount} dataKeyX="brand" dataKeyY="count" />
+          <BrandsChart<IAveragePriceOfEachBrand> queryKey="averagePrice" queryFn={fetchAveragePriceOfEachBrand} dataKeyX="brand" dataKeyY="price" />
+          <BrandsChart<IBrandAverageRatingNumberReviews> queryKey="brandAverageRatingNumberReviews" queryFn={fetchBrandAverageRatingNumberReviews} dataKeyX="brand" dataKeyY="averageRating" />
+        </Grid>
+      </Flex>
     </AppProvider>
   );
 }
