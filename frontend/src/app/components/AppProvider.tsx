eaa8a4581383232d3ed7f4bd572eb33a1b13be61
MatheusDelduque <matheusdelduque1@gmail.com>
2024-08-07 00:01:57 -0300

    refactor: improve code structure

--- frontend/src/app/components/AppProvider.tsx
+++ frontend/src/app/components/AppProvider.tsx
@@ -2,9 +2,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import { useState } from "react";
 
 export const AppProvider = ({children}: {children: React.ReactNode}) => {
-    const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
     return (
       <QueryClientProvider client={queryClient}>
